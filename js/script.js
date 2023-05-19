032dca1810b223ed90b13c3d3a9d6c8bd96eef13
Amanda <amanda770328@gmail.com>
2023-05-19 11:38:16 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,6 +21,11 @@ $(document).ready(function () {
 	$('#navToggle,.overlapblackbg').on('click', function(){
 		$('.wsmenucontainer').toggleClass( "mrginleft" );
 	});
+
+	$(".goTop").click(function(){
+		$("html,body").animate({scrollTop:0},600);
+		return false;
+	});
 	
 	$(window).scroll(function(){
 		var scrollTop = $(window).scrollTop();
@@ -41,65 +46,4 @@ $(document).ready(function () {
 		}
 	});
 	new WOW().init();
-
-	$(".slider_prd").slick({
-		dots: false,
-		infinite: true,
-		arrows: false,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: 0,
-		speed: 8500,
-		cssEase: "linear",
-		pauseOnHover: false,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-		{
-		breakpoint: 1280,
-		settings: {
-			slidesToShow: 3,
-			slidesToScroll: 3,
-		}
-		},
-		{
-		breakpoint: 992,
-		settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2,
-		}
-		},
-		{
-		breakpoint: 480,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-		}
-		}
-	]
-	});
-
-	$('.gallery').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: 0,
-		speed: 7000,
-		cssEase: "linear",
-		arrows: false,
-		dots: false,
-		pauseOnHover: false,
-		responsive: [{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1
-			}
-		}, {
-			breakpoint: 520,
-			settings: {
-				slidesToShow: 1,
-			}
-		}]
-	});
 });
