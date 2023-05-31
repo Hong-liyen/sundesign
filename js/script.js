908e408beb43763a21e4877f51115ac892b9a937
Amanda <amanda770328@gmail.com>
2023-05-31 21:39:51 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,4 +46,20 @@ $(document).ready(function () {
 		}
 	});
 	new WOW().init();
-});
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});});
